a213bfa5d48560856706edd2f07211c5ab4303ea
Michael Stewart <stewarmc@jmu.edu>
2021-01-07 00:39:33 -0500

    example from docs

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,36 @@ import React from 'react';
 import logo from './logo.svg';
 import { Counter } from './features/counter/Counter';
 import './App.css';
+import WebMidi from 'webmidi'
 
+
+(window as any).WebMidi = WebMidi
 function App() {
+
+  // Enable WebMidi.js
+  WebMidi.enable(function (err) {
+
+    if (err) {
+      console.log('1', "WebMidi could not be enabled.", err);
+    }
+
+    // Viewing available inputs and outputs
+    console.log('2', WebMidi.inputs);
+    console.log('3', WebMidi.outputs);
+
+    // Reacting when a new device becomes available
+    WebMidi.addListener("connected", function (e) {
+      console.log('4', e);
+      if (e.port.type === "output") {
+        e.port.playNote("C3")
+      }
+    });
+
+    // Reacting when a device becomes unavailable
+    WebMidi.addListener("disconnected", function (e) {
+      console.log('5', e);
+    });
+  })
   return (
     <div className="App">
       <header className="App-header">
